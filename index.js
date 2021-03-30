4f4ce7254c3394f5069c872a0765a87256fdab79
WalukaUI <cwaluka@yahoo.com>
2021-03-29 22:35:29 -0700

    10 Passing

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser="waluka";
+const welcomeMessage=`Welcome to Flatbook, " ${currentUser}+!`;
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+const slicer=currentUser.slice(0,1);
+const shortGreeting=`Welcome, ${slicer}!`;
